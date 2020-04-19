e10f0b7c13e912d45a5d25edbb3536e766851563
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-19 10:50:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  hash.collect do |key1, value1|
+    if 
 end
+
+
+birthday_kids = {
+    "Timmy" => 9, 
+    "Sarah" => 6, 
+    "Amanda" => 27
+}
+ 
+birthday_kids.collect do |kids_name, age|
+    age
+end
+ 
+# => [9, 6, 27]
